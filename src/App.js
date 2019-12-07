46f98245a0b9617342107f424d3f4dc3f02617c0
YevhenSoldatov1998 <arr.ess1998@gmail.com>
2019-12-07 10:25:26 +0300

    package modified

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import _ from 'lodash/lodash.min'
 import './App.css';
 
 class App extends React.Component {
@@ -13,14 +14,48 @@ class App extends React.Component {
     const response = await fetch(`http://www.filltext.com/?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName
     %7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D`)
     const data = await response.json()
-    this.setState({dataJSON: data})
+    this.setState({
+        dataJSON: data,
+        sort: 'ask'
+    })
   }
   handleSort = (str)=>{
-    console.log(str)
-    console.log(this.state.dataJSON)
-    for(let item in this.state.dataJSON){
-      console.log(this.state.dataJSON.item)
-    }
+      if (str === 'id') {
+              let dataID = this.state.sort == 'desk'? (this.state.dataJSON.sort((a, b) => a.id > b.id ? 1 : -1)):(this.state.dataJSON.sort((a, b) => a.id < b.id ? 1 : -1))
+              this.setState({
+                  dataJSON: dataID,
+                  sort: this.state.sort == 'desk'?"asc":"desk"
+          })
+
+      }
+      else if (str === 'address.streetAddress') {
+          let dataID = this.state.sort == 'desk'? (this.state.dataJSON.sort((a, b) => a.address.streetAddress > b.address.streetAddress ? 1 : -1)):(this.state.dataJSON.sort((a, b) => a.address.streetAddress < b.address.streetAddress ? 1 : -1))
+          this.setState({
+              dataJSON: dataID,
+              sort: this.state.sort == 'desk'?"asc":"desk"
+          })
+      }
+      else if (str === 'lastName') {
+          let dataID = this.state.sort == 'desk'? (this.state.dataJSON.sort((a, b) => a.lastName > b.lastName ? 1 : -1)):(this.state.dataJSON.sort((a, b) => a.lastName < b.lastName ? 1 : -1))
+          this.setState({
+              dataJSON: dataID,
+              sort: this.state.sort == 'desk'?"asc":"desk"
+          })
+      }
+      else if (str === 'email') {
+          let dataID = this.state.sort == 'desk'? (this.state.dataJSON.sort((a, b) => a.email > b.email ? 1 : -1)):(this.state.dataJSON.sort((a, b) => a.email < b.email ? 1 : -1))
+          this.setState({
+              dataJSON: dataID,
+              sort: this.state.sort == 'desk'?"asc":"desk"
+          })
+      }
+      else if (str === 'phone') {
+          let dataID = this.state.sort == 'desk'? (this.state.dataJSON.sort((a, b) => a.phone > b.phone ? 1 : -1)):(this.state.dataJSON.sort((a, b) => a.phone < b.phone ? 1 : -1))
+          this.setState({
+              dataJSON: dataID,
+              sort: this.state.sort == 'desk'?"asc":"desk"
+          })
+      }
   }
   render() {
         return (
@@ -29,11 +64,11 @@ class App extends React.Component {
                 <table className="table">
                     <thead>
                     <tr>
-                        <th scope="col" onClick={this.handleSort.bind(null , 'id')}>ID</th>
-                        <th scope="col" onClick={this.handleSort.bind(null , 'address.streetAddress')}>ADDRESS</th>
-                        <th scope="col" onClick={this.handleSort.bind(null , 'lastName')}>LAST NAME</th>
-                        <th scope="col" onClick={this.handleSort.bind(null , 'email')}>EMAIL</th>
-                        <th scope="col" onClick={this.handleSort.bind(null , 'phone')}>PHONE</th>
+                        <th scope="col" onClick={() => this.handleSort('id')}>ID <span>{this.state.sort}</span></th>
+                        <th scope="col" onClick={() => this.handleSort('address.streetAddress')}>ADDRESS</th>
+                        <th scope="col" onClick={() => this.handleSort('lastName')}>LAST NAME</th>
+                        <th scope="col" onClick={() => this.handleSort( 'email')}>EMAIL</th>
+                        <th scope="col" onClick={() => this.handleSort( 'phone')}>PHONE</th>
                     </tr>
                     </thead>
                     <tbody>
